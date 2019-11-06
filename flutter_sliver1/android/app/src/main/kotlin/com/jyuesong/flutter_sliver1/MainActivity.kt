fece775c98efd8bb765f0ea538ca809836a39cd8
jyuesong <425698907@qq.com>
2019-11-06 20:57:57 +0800

    add sliver1

---
+++ flutter_sliver1/android/app/src/main/kotlin/com/jyuesong/flutter_sliver1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jyuesong.flutter_sliver1
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
